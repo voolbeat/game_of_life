f04bf23bc61f95deafa8e98813da13bfcf9f30ad
voolbeat <mishan-97@mail.ru>
2019-06-27 21:13:59 +0300

    num_neighbours + get_next_state

--- main.cpp
+++ main.cpp
@@ -26,4 +26,27 @@ int main(void)
 	return 0;
 }
 
+int get_next_state(int x, int y) {
+	// получим количество соседей и состояние данной клетки
+	int neighbors = num_neighbors(x, y), state = get_cell_state(x, y);
 
+	// проверим условия из правил игры, когда клетка жива
+	if (state == 0 && neighbors == 3) return ALIVE;
+	if (state == 1 && (neighbors == 2 || neighbors == 3)) return ALIVE;
+	
+	//во всех остальных случаях клетка мертва
+	return DEAD;
+}
+
+int num_neighbors(int x, int y) {
+	int count = 0;
+	// сдвиги, чтобы получить соседей от данной клетки х у
+	int offset[8][2] = { { -1,-1 },{ -1,0 },{ -1,1 },
+	{ 0,1 },{ 0,-1 },
+	{ 1,-1 },{ 1,0 },{ 1,1 } };
+	// пробежимся по всем 8 соседям и посчитаем количество живых соседей в переменную count
+	for (int i = 0; i < 8; i++) {
+		count = count + get_cell_state(x + offset[i][0], y + offset[i][1]);
+	}
+	return count;
+}
